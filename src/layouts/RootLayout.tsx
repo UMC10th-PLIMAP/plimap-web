9bed24736e1a782e2333886e89857ddf8309d2b6
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-06-30 08:34:07 +0900

    feat : 공통 레이아웃

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+  return (
+    <div className="w-[402px] h-screen mx-auto ">
+      <Outlet />
+    </div>
+  );
+};
+
+export default RootLayout;
