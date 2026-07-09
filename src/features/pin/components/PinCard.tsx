a48d2c980525e277e88b98d42069b321d06ed940
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-07-09 22:02:17 +0900

    rename : PinCard 컴포넌트로 수정

--- src/features/pin/components/PinCard.tsx
+++ src/features/pin/components/PinCard.tsx
@@ -1,24 +1,25 @@
-import { ChevronRight, Heart } from 'lucide-react';
-
 import { cn } from '@/lib/utils';
 import type { PinSong } from '@/types/pin';
 
-type PinSongCardProps = {
+import NextIcon from '@/assets/icons/next.svg?react';
+import HeartIcon from '@/assets/icons/heart.svg?react';
+
+type PingCardProps = {
   pin: PinSong;
   onClick?: () => void;
   className?: string;
 };
 
-export function PinSongCard({ pin, onClick }: PinSongCardProps) {
+export function PinCard({ pin, onClick }: PingCardProps) {
   const { title, artist, pinCount, likeCount, liked } = pin;
 
   return (
     <button
       type="button"
       onClick={onClick}
-      className="flex w-full gap-3 rounded-xl bg-pli-black-75 p-[18px] justify-between"
+      className="flex w-full rounded-xl bg-pli-black-85 p-4.5 justify-between"
     >
-      <div className="flex flex-col  gap-4">
+      <div className="flex flex-col gap-4">
         <div className="flex gap-4">
           <div className="size-13 rounded-lg bg-grayscale-0" />
 
@@ -29,7 +30,7 @@ export function PinSongCard({ pin, onClick }: PinSongCardProps) {
         </div>
 
         <div className="flex items-center gap-1 body-15-r text-grayscale-400">
-          <Heart
+          <HeartIcon
             className={cn('size-4', liked ? 'fill-red text-red' : 'text-grayscale-400')}
             aria-hidden
           />
@@ -39,7 +40,7 @@ export function PinSongCard({ pin, onClick }: PinSongCardProps) {
 
       <div className="flex items-center gap-1 etc-13-r text-grayscale-400">
         {pinCount}명이 PIN
-        <ChevronRight className="size-4" aria-hidden />
+        <NextIcon className="size-4" aria-hidden />
       </div>
     </button>
   );
