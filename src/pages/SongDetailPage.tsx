384ce324151195766713183478222992f1a9d53a
kimtaewoo <70637743+kim3360@users.noreply.github.com>
2026-08-10 21:14:38 +0900

    fix : 곡 상세 노치 겹치는 부분 수정

--- src/pages/SongDetailPage.tsx
+++ src/pages/SongDetailPage.tsx
@@ -572,7 +572,7 @@ export default function SongDetailPage() {
               className="pointer-events-none absolute inset-x-0 bottom-0 h-24 bg-gradient-to-b from-pli-black-100/0 to-pli-black-100"
             />
 
-            <div className="relative z-10 flex h-full flex-col">
+            <div className="relative z-10 flex h-full flex-col pt-[env(safe-area-inset-top)]">
               <div className="flex h-[64px] items-center justify-between px-4">
                 <button
                   type="button"
